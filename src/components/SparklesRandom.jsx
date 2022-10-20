c44a9b60c662235909dc74b0f3f7d20bdca13012
anhdoursa <anh.do@ursa.vn>
2022-10-20 15:12:10 +0700

    fix

--- src/components/SparklesRandom.jsx
+++ src/components/SparklesRandom.jsx
@@ -2,10 +2,11 @@ import { Sparkles } from '@react-three/drei';
 import React, { useMemo } from 'react';
 
 const SparklesRandom = ({ count, size, random, ...props }) => {
+  if (!count && count !== 0) count = 100;
+  if (!size && size !== 0) size = 5;
   const sizes = useMemo(() => {
     return Array.from({ length: count }, () => size * Math.random());
-  }, [size, count]);
-  console.log(sizes);
+  }, [count, size]);
   return <Sparkles {...props} count={count} size={random ? sizes : size} />;
 };
 
